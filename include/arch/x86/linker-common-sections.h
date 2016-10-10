Alvin-Lau/zephyr

net: yaip: Add a neighbor cache needed in IPv6
This is the framework that neighbor discovery uses. The ND
is coming in later commits.

Change-Id: Iaaa67c80c0b6b8a3adb9217413b906e0a22d3920
Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

include/arch/x86/linker-common-sections.h
@@ -212,6 +212,14 @@ SECTIONS
 		KEEP(*(SORT_BY_NAME(".net_if*")))
 		__net_if_end = .;
 	} GROUP_LINK_IN(RAM)
+
+	SECTION_PROLOGUE(net_nbr, (OPTIONAL),)
+	{
+		__net_nbr_start = .;
+		*(".net_nbr.*")
+		KEEP(*(SORT_BY_NAME(".net_nbr*")))
+		__net_nbr_end = .;
+	} GROUP_LINK_IN(RAM)
 #endif
 
 	SECTION_PROLOGUE(_k_task_list, ALIGN(4), ALIGN(4))

yboutreux/zephyr_stm32mp1

include/arch/x86/linker-common-sections.h
@@ -163,6 +163,14 @@ SECTIONS
 		KEEP(*(SORT_BY_NAME(".net_if*")))
 		__net_if_end = .;
 	} GROUP_DATA_LINK_IN(RAM, ROM)
+
+	SECTION_DATA_PROLOGUE(net_nbr, (OPTIONAL),)
+	{
+		__net_nbr_start = .;
+		*(".net_nbr.*")
+		KEEP(*(SORT_BY_NAME(".net_nbr*")))
+		__net_nbr_end = .;
+	} GROUP_DATA_LINK_IN(RAM, ROM)
 #endif
 
 	SECTION_DATA_PROLOGUE(_k_task_list, (OPTIONAL),)