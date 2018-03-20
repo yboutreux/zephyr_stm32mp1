cc6e5cea2f1b4b5d56266cfbbe6f7c07464d826e
Kumar Gala <kumar.gala@linaro.org>
2018-03-21 05:35:21 +0800

    sample: openamp: cleanup cmake to make building easier
    Make changes to cmake to build libmetal & openamp with the assumption
    that the dir structure is:
    
    <ROOT>/zephyr
    <ROOT>/libmetal
    <ROOT>/open-amp
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ cmake/open-amp.cmake
@@ -0,0 +1,26 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+  libmetal                 # Name for custom target
+  SOURCE_DIR $ENV{ZEPHYR_BASE}/../libmetal/
+  INSTALL_COMMAND ""      # This particular build system has no install command
+  CMAKE_ARGS -DWITH_ZEPHYR=ON -DBOARD=${BOARD}
+  )
+
+ExternalProject_Get_property(libmetal BINARY_DIR)
+set(LIBMETAL_INCLUDE_DIR ${BINARY_DIR}/lib/include)
+set(LIBMETAL_LIBRARY     ${BINARY_DIR}/lib/libmetal.a)
+
+ExternalProject_Add(
+  open-amp
+  SOURCE_DIR $ENV{ZEPHYR_BASE}/../open-amp/
+  DEPENDS libmetal
+  INSTALL_COMMAND ""      # This particular build system has no install command
+  CMAKE_ARGS -DWITH_ZEPHYR=ON -DWITH_PROXY=OFF -DBOARD=${BOARD} -DLIBMETAL_INCLUDE_DIR=${LIBMETAL_INCLUDE_DIR} -DLIBMETAL_LIB=${LIBMETAL_LIBRARY}
+)
+
+ExternalProject_Get_property(open-amp SOURCE_DIR)
+set(OPENAMP_INCLUDE_DIR  ${SOURCE_DIR}/lib/include CACHE PATH "Path to the OpenAMP header files")
+ExternalProject_Get_property(open-amp BINARY_DIR)
+set(OPENAMP_LIBRARY      ${BINARY_DIR}/lib/libopen_amp.a CACHE FILEPATH "Path to the OpenAMP library")
+
